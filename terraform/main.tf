60ceb5c671b10d1bd671a42df978af0528e372c4
kasssas <elkassasmostafa29@gmail.com>
2026-02-08 22:14:23 +0200

    Fix EC2 volume size: increase from 8GB to 30GB for AL2023 AMI compatibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -181,7 +181,7 @@ resource "aws_instance" "web" {
   }
 
   root_block_device {
-    volume_size           = 8
+    volume_size           = 30
     volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
